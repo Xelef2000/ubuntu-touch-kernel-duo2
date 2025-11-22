41c6a73f0d6a714e3d6d2a8597020e47e4b8196e
Felix <felix@niederer.dev>
2025-11-22 14:32:46 +0000

    fixed vendor boot creation

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -45,14 +45,19 @@ KERNEL_BOOTIMAGE_CMDLINE = console=ttyMSM0,115200n8 log_buf_len=256K earlycon=ms
 # For Header v3, DTB is NOT appended to kernel (Image.gz-dtb), 
 # it is passed as a standalone file to vendor_boot.
 
-KERNEL_IMAGE_WITH_DTB = 0
+KERNEL_IMAGE_WITH_DTB = 1
 KERNEL_IMAGE_WITH_DTB_OVERLAY = 0
 KERNEL_IMAGE_WITH_DTB_OVERLAY_IN_KERNEL = 0
 
 # --- ACTIVE DTB SELECTION ---
 # Using the Source-built DTB 
-KERNEL_BOOTIMAGE_DTB = /buildd/sources/out/KERNEL_OBJ/arch/arm64/boot/dts/vendor/qcom/lahaina-v2.1.dtb
-KERNEL_IMAGE_DTB = arch/arm64/boot/dts/vendor/qcom/lahaina-v2.1.dtb
+# KERNEL_BOOTIMAGE_DTB = /buildd/sources/out/KERNEL_OBJ/arch/arm64/boot/dts/vendor/qcom/lahaina-v2.1.dtb
+# KERNEL_IMAGE_DTB = arch/arm64/boot/dts/vendor/qcom/lahaina-v2.1.dtb
+# KERNEL_BOOTIMAGE_DTB = device/microsoft/surface-duo2/prebuilts/stock_duo2.dtb
+# KERNEL_IMAGE_DTB = device/microsoft/surface-duo2/prebuilts/stock_duo2.dtb
+
+KERNEL_BOOTIMAGE_DTB = device/microsoft/surface-duo2/prebuilts/stock_duo2.dtb
+# KERNEL_DTB = vendor/qcom/lahaina-v2.1.dtb
 
 # --- PREBUILT REFERENCES ---
 # If using prebuilt, ensure this file exists in your device tree:
